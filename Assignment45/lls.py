fc228e0bfdd05767dc33b7f9c276ae0b4dd7e8f3
fahimecodes <fahime.mir75@gmail.com>
2023-07-31 01:08:54 +0330

    Assignment 44 processing

---
+++ Assignment45/lls.py
@@ -0,0 +1,28 @@
+import numpy as np
+from numpy.linalg import inv
+
+class LinearLeastSquare:
+    def __init__(self):
+        # self.w = None
+        pass
+
+    def fit(self, X_train, Y_train):
+
+        # train
+        self.w = inv(X_train.T @ X_train) @ X_train.T @ Y_train
+
+
+    def predict(self, X_test):
+        #test
+        Y_pred = X_test @ self.w
+        return Y_pred
+
+    def evaluate(self, X_test, Y_test, metric):
+        Y_pred = self.predict(X_test)
+
+        if metric == "mae":
+            loss = np.sum(np.abs(Y_test - Y_pred)) / len(Y_test)
+        elif metric == "mse":
+            loss = np.sum((Y_test - Y_pred)**2) / len(Y_test)
+
+        return loss
